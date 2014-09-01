2780f8d0e175ab7ef29ab160e79f28e936720d10
zkbreeze <zk.kyoto@gmail.com>
2014-09-01 15:44:19 +0900

    add kvsmake.conf

--- HeatSphere/PBR/run_prism.sh
+++ HeatSphere/PBR/run_prism.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./PBR -u-prism-ball /Users/Breeze/Data/HeatSphere/cut_prism_volume/51000.kvsml -s 6 -o 0.1
+#./PBR -u-prism-ball /Users/Breeze/Data/HeatSphere/cut_prism_volume/51000.kvsml -s 6 -o 0.1
+./PBR -u-prism-ball /Volumes/WORK_1TB/heatSphere/cut_prism_volumedata/51000.kvsml -s 6 -o 0.1
