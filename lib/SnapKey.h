81b2b3db7071e04adf1bac44d30371026185e027
zkbreeze <zk.kyoto@gmail.com>
2014-09-04 16:14:23 +0900

    revise the header of the lib

--- lib/SnapKey.h
+++ lib/SnapKey.h
@@ -1,6 +1,19 @@
+//
+//  SnapKey.h
+//
+//
+//  Created by Kun Zhao on 2014-09-04 16:12:21.
+//
+//
+
+#ifndef KUN__SNAPKEY_H_INCLUDE
+#define KUN__SNAPKEY_H_INCLUDE
+
 #include <kvs/KeyPressEventListener>
 
 class SnapKey : public kvs::KeyPressEventListener 
 {
 	void update( kvs::KeyEvent* event );
 };
+
+#endif // KUN__SNAPKEY_H_INCLUDE
