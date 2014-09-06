b735cfd1280cd5bddf44833dd5f41cd02ce96d15
zkbreeze <zk.kyoto@gmail.com>
2014-09-06 11:17:28 +0900

    fit prism

--- App/PBR/main.cpp
+++ App/PBR/main.cpp
@@ -228,6 +228,10 @@ int main( int argc, char** argv )
 
 	renderer->setBaseOpacity( ::base_opacity );
 	screen.registerObject( point, renderer );
+	screen.setBackgroundColor( kvs::RGBColor( 255, 255, 255 ) );
+	// enlarge the view
+	if( param.hasOption( "u-prism-ball" ) )
+		screen.scene()->camera()->translate( kvs::Vec3( 0.0, 0.0, -5.0 ) );
 	screen.show();
 
 	// Set the transfer function editor
