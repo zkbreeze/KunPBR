b54685f5dc9f637d564334c2dbcde77f34389118
zkbreeze <zk.kyoto@gmail.com>
2015-12-14 17:53:47 +0900

    revisions

---
+++ Bin/run_animationtsunami/adv.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# run_animationtsunami
+# Default Mode
+# ./Tsunami -adv ~/Data/1f_in/r02/output_advone_00050.adv -rep 81 -l ~/Data/1f_in/151125/f1_inside_20141126/f1_inside_20141126.obj
+./AnimationTsunami -adv ~/Data/1f_in/r02/ -v ~/Data/1f_in/f1_inside_20141126.vtk -rep 200 -nsteps 10
